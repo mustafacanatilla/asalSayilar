fb27497664add364c98cb2f5c626d228d9b211de
Mustafa Can Atilla <mustafacanatilla34@hotmail.com>
2022-08-30 08:28:27 +0300

    asalSayilar

---
+++ src/asalSayiBulma.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class asalSayiBulma {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            boolean asal = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0)
+                    asal = false;
+            }
+            if (asal) {
+                System.out.print(i + ",");
+            }
+        }
+
+    }
+}
